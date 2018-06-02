f307386cec76911c2ae452a27a28219d09329c35
xemasiv <xemasiv@gmail.com>
2018-06-02 23:45:46 +0800

    + babel-preset-flow babel-core -D

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,9 @@
 // @flow
+/* eslint-disable no-console */
 import 'babel-polyfill';
 
-const log = something => something;
+const log = something => console.log(something);
 
 const x :string = '123';
 
 log(x);
-const O = {};
-log(Reflect.defineMetadata('foo', 'bar', O));
-log(Reflect.ownKeys(O)); // => []
-log(Reflect.getOwnMetadataKeys(O)); // => ['foo']
-log(Reflect.getOwnMetadata('foo', O)); // => 'bar'
